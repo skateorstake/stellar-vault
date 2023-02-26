3f4cd97b97d4a99d8c34982511e8d330802af8cc
skateorstake <viwjocowik@outlook.com>
2023-02-26 14:00:01 +0200

    Integrate zk-rollups for scaling

--- oracle.go
+++ oracle.go
@@ -1,3 +1,4 @@
 // Implement zero-knowledge proof (ZKP) support
 // Implement rewards escrow system
 // Refactor incentive structure in DeFi protocol
+// Refactor incentive structure in DeFi protocol
