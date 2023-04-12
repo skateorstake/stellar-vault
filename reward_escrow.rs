dc312cee34d72d986573ae5bfef33123b7dbc651
skateorstake <viwjocowik@outlook.com>
2023-04-12 14:00:00 +0200

    Refactor reward pooling algorithm

--- reward_escrow.rs
+++ reward_escrow.rs
@@ -1,3 +1,4 @@
 // Integrate with decentralized prediction markets
 // Fix interoperability issues with other blockchains
 // Add support for decentralized lending platforms
+// Refactor reward pooling algorithm
