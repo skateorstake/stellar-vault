9819f80fb2c830867c29ec500c2b061e7aca0d2e
skateorstake <viwjocowik@outlook.com>
2023-04-19 14:00:00 +0200

    Fix issues in multi-token smart contracts

--- reward_escrow.rs
+++ reward_escrow.rs
@@ -2,3 +2,4 @@
 // Fix interoperability issues with other blockchains
 // Add support for decentralized lending platforms
 // Refactor reward pooling algorithm
+// Optimize contract storage for gas reduction
