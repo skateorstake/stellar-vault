138f2e59a960c825f55fb9a2ca293d895cba0f1c
skateorstake <viwjocowik@outlook.com>
2023-08-03 14:00:01 +0200

    Fix bugs in NFT metadata handling

--- peer_to_peer_banking_protocols.go
+++ peer_to_peer_banking_protocols.go
@@ -1,3 +1,4 @@
 // Upgrade to latest Solidity version
 // Add support for decentralized credit scoring
 // Fix compatibility issues with major wallets
+// Fix type mismatch in Haskell smart contract
