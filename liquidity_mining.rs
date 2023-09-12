3c8c5a1c3be5583dd9993a1e01b4d8e1c1baa870
skateorstake <viwjocowik@outlook.com>
2023-09-12 14:00:00 +0200

    Refactor reward pooling algorithm

--- liquidity_mining.rs
+++ liquidity_mining.rs
@@ -1,3 +1,4 @@
 // Fix errors in contract interaction with wallets
 // Enhance smart contract compatibility
 // Refactor incentive structure in DeFi protocol
+// Implement reward distribution feature
