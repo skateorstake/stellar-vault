2f4f3c62d592bac025b523c39a2cc2fec59606b1
skateorstake <viwjocowik@outlook.com>
2023-10-21 14:00:01 +0200

    Optimize WASM compilation for Go contracts

--- voting_mechanism.rs
+++ voting_mechanism.rs
@@ -1,3 +1,4 @@
 // Fix oracle data delays in decentralized apps
 // Add support for zk-SNARKs in smart contracts
 // Integrate with decentralized storage systems
+// Enhance contract initialization process
