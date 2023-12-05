231087e5983b3eacc94bc8f43f5f2355f0b7b539
skateorstake <viwjocowik@outlook.com>
2023-12-05 14:00:01 +0200

    Refactor multi-signature wallet code

--- staking.py
+++ staking.py
@@ -1,3 +1,4 @@
 // Code cleanup and documentation update
 // Implement decentralized arbitration in contracts
 // Refactor storage layout for better efficiency
+// Fix compatibility issues with major wallets
