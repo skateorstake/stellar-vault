5e8667388b6b4bc2b5d2a760f90b5c7d9e605bc7
skateorstake <viwjocowik@outlook.com>
2024-01-02 14:00:00 +0200

    Fix ERC-721 contract issues with transfers

--- cryptographic_signing_solutions.py
+++ cryptographic_signing_solutions.py
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Fix deserialization bug in Haskell code
 // Fix issues with gas optimization in smart contracts
+// Improve staking pool reward management
