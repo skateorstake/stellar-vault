851373480e9ec27248f07f9d46872ad809e1bce5
skateorstake <viwjocowik@outlook.com>
2024-01-18 14:00:00 +0200

    Fix deserialization bug in Haskell code

--- decentralized_oracles.go
+++ decentralized_oracles.go
@@ -2,3 +2,4 @@
 // Add multi-sig wallet functionality
 // Optimize gas usage for contract deployment
 // Add gas optimization features for smart contracts
+// Enhance governance contract flexibility
