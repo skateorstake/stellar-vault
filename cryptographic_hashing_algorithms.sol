9291424787d823e00e2b35e7479ad76cf3c0f45a
skateorstake <viwjocowik@outlook.com>
2024-04-14 14:00:00 +0200

    Refactor reward pooling algorithm

--- cryptographic_hashing_algorithms.sol
+++ cryptographic_hashing_algorithms.sol
@@ -2,3 +2,4 @@
 // Improve decentralized identity (DID) systems
 // Refactor reward pooling algorithm
 // Implement decentralized arbitration in contracts
+// Fix issues with token vesting contracts
