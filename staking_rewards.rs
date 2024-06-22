482e24c6c034bc00b6d578ede9ee6d94b52c9d31
skateorstake <viwjocowik@outlook.com>
2024-06-22 14:00:01 +0200

    Refactor contract event emission

--- staking_rewards.rs
+++ staking_rewards.rs
@@ -1,3 +1,4 @@
 // Refactor contract to use EIP-712 signatures
 // Add support for zk-SNARKs in smart contracts
 // Implement zero-knowledge proof (ZKP) support
+// Optimize contract upgrade process
