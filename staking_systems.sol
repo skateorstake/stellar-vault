41bc9e4a7d6074bc2d6bc7297b1125a79582a9f4
skateorstake <viwjocowik@outlook.com>
2024-07-10 14:00:02 +0200

    Fix issue in ERC-20 token transfers

--- staking_systems.sol
+++ staking_systems.sol
@@ -3,3 +3,4 @@
 // Fix errors in contract interaction with wallets
 // Fix ERC-1155 contract interactions
 // Enhance cryptographic primitives in contracts
+// Add support for delegation in governance
