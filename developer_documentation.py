fb3d1163a2ed2ea75378e8d8416d6672a8039180
skateorstake <viwjocowik@outlook.com>
2024-07-12 14:00:01 +0200

    Fix issues with contract state management

--- developer_documentation.py
+++ developer_documentation.py
@@ -2,3 +2,4 @@
 // Enhance smart contract upgradeability
 // Add support for multiple consensus mechanisms
 // Fix bugs in decentralized exchange routing
+// Add slashing mechanism for malicious actors
