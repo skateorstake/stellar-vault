daedcdad6046cc6c64076c035a279333fa558930
skateorstake <viwjocowik@outlook.com>
2024-08-08 14:00:01 +0200

    Refactor reward pooling algorithm

--- token_standard_extensions.sol
+++ token_standard_extensions.sol
@@ -1,3 +1,4 @@
 // Fix token transfer approval issues
 // Fix compatibility issues with major wallets
 // Improve decentralized lending contract flexibility
+// Implement automated governance voting
