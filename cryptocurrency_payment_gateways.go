30e79e7c61a5f699446b16a077eba221823dd059
skateorstake <viwjocowik@outlook.com>
2024-11-03 14:00:01 +0200

    Optimize token minting flow

--- cryptocurrency_payment_gateways.go
+++ cryptocurrency_payment_gateways.go
@@ -1,3 +1,4 @@
 // Fix issues with gas optimization in smart contracts
 // Add support for Layer-2 solutions
 // Fix governance proposal approval logic
+// Fix security vulnerability in random number generation
