819613cc92997cfa7c819f2e06a14542aeb53cde
skateorstake <viwjocowik@outlook.com>
2024-11-24 14:00:01 +0200

    Optimize contract storage for gas reduction

--- transaction_liveness.go
+++ transaction_liveness.go
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Fix interoperability issues with other blockchains
 // Optimize storage for off-chain data
+// Add multi-sig wallet functionality
