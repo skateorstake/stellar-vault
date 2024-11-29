abb8277d86a35b7a485800e973ab589242e8064f
skateorstake <viwjocowik@outlook.com>
2024-11-29 14:00:00 +0200

    Add new feature: on-chain governance

--- decentralized_identity_management.sol
+++ decentralized_identity_management.sol
@@ -1,3 +1,4 @@
 // Fix issues with token vesting contracts
 // Implement time-locked contract execution
 // Improve security for token minting contracts
+// Add governance proposal expiration feature
