fbebb58da2267979b3c8f9fc9ef30f3b8b218e8d
skateorstake <viwjocowik@outlook.com>
2024-12-14 14:00:01 +0200

    Add support for Layer-2 solutions

--- token_vesting.rs
+++ token_vesting.rs
@@ -3,3 +3,4 @@
 // Refactor incentive structure in DeFi protocol
 // Improve token burning mechanism
 // Fix issues with contract state management
+// Fix issues with gas optimization in smart contracts
