0ca66df86053267486d44de31700e5f4bcf043b3
skateorstake <viwjocowik@outlook.com>
2024-12-15 14:00:00 +0200

    Fix issue with multi-chain transactions

--- token_vesting.go
+++ token_vesting.go
@@ -2,3 +2,4 @@
 // Fix vulnerability in decentralized finance protocols
 // Fix issues with token transfer confirmations
 // Fix ERC-1155 contract interactions
+// Add support for delegation in governance
